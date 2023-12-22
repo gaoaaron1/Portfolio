deba93583aa70fc1d03552b05d0837cdd488a0d4
Aaron Gao <gaoaaron1@gmail.com>
2023-12-22 10:43:31 -0700

    Added Projects

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Header from './components/header/Header';
 import Nav from './components/navigation/Nav';
 import About from './components/about/About';
 import Experience from './components/experience/Experience';
+import Projects from './components/projects/Projects';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Nav />
       <About />
       <Experience />
+      <Projects />
     </>
   );
 }
---
+++ src/components/projects/Projects.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import './Projects.css';
+const IMG1 = require('../../assets/portfolio1.png');
+
+interface ProjectsItem {
+  id: number;
+  image: string;
+  title: string;
+  github: string;
+  demo: string;
+}
+
+// ARRAY OF OBJECTS
+
+const data: ProjectsItem[] = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Captain Kenyu',
+    github: 'https://github.com/gaoaaron1/Captain-Kenyu-Old',
+    demo: 'https://play.google.com/store/apps/details?id=com.aaron.game&gl=US'
+  }
+];
+
+const Projects: React.FC = () => {
+  return (
+    <section id='projects'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container projects__container">
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className='projects__item'>
+              <div className="projects__item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="projects__item-cta">
+                <a href={github} className='btn'>Github</a>
+                <a href={demo} className='btn btn-primary' target='_blank' rel='noopener noreferrer'>Live Demo</a>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
